a7fb54fbc3138076bed645ef9de9dad6a819eb6d
Mohamed2212008 <mhs2212000@gmail.com>
2025-08-18 14:21:57 +0300

    fixing files bug

--- models/trainee.py
+++
@@ -1,8 +0,0 @@
-class Trainee:
-    def __init__(self, name, phone_num, sport, sub_price, notes):
-        self.name = name
-        self.phone_num = phone_num
-        self.sport = sport
-        self.sub_price = sub_price
-        self.notes = notes
- 
